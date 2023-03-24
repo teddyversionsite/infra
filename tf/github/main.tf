679d3f8d6651dd2010224e16fb7fe443aaaf4d3f
tylerrasor <trasor@gmail.com>
2023-03-24 18:17:41 -0400

    add user-service

--- tf/github/main.tf
+++ tf/github/main.tf
@@ -69,3 +69,30 @@ resource "github_branch_protection" "teddyversion-frontend" {
     ]
   }
 }
+
+resource "github_repository" "user-service" {
+  name = "user-service"
+
+  visibility = "public"
+
+  template {
+    owner                = var.org
+    repository           = "user-service"
+    include_all_branches = true
+  }
+}
+
+resource "github_branch_protection" "user-service" {
+  repository_id = github_repository.user-service.name
+
+  pattern        = "main"
+  enforce_admins = true
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+    pull_request_bypassers = [
+      "/tylerrasor"
+    ]
+  }
+}
